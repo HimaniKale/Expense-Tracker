5524779d6482d7cdb4a37df0e31e001f28052600
HimaniKale <146484935+HimaniKale@users.noreply.github.com>
2024-04-06 12:18:26 +0530

    Update trial.py

--- webapp/trial.py
+++ webapp/trial.py
@@ -2,9 +2,21 @@ from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
+@app.route("/home")
+def test():
     return render_template("temp.html")
+
+@app.route("/setb")
+def test1():
+    return render_template("new.html")
+
+@app.route("/expense")
+def test2():
+    return render_template("addexp.html")
+
+@app.route("/profile")
+def test3():
+    return render_template("profile.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
